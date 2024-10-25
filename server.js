b60253abc351f8c4bc0d5fd9616a4f66a56cde14
Aryan Manu <aryanmanu@Aryans-MacBook-Air-2.local>
2024-10-25 12:05:18 +0530

    i

--- server.js
+++ server.js
@@ -90,5 +90,5 @@ app.post('/api/emotions', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
